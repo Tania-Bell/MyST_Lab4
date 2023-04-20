f2d2b88836a35297c7961c5a203002d4eafd2f1e
Tania-Bell <taniabell1@hotmail.com>
2023-04-19 21:22:26 -0600

    creacion de dataframes

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import asyncio
 import ccxt.async_support as ccxta
+import ccxt
 import time
 import numpy as np
 import pandas as pd
+import ast
 
 
 async def async_client(exchange_id, run_time: int, symbol: str):
@@ -16,11 +18,16 @@ async def async_client(exchange_id, run_time: int, symbol: str):
             await exchange.load_markets()
             market = exchange.market(symbol)
             orderbook = await exchange.fetch_order_book(market["symbol"])
+            ohlcv = await exchange.fetch_ohlcv(market["symbol"], limit=1)
+            closing_price = ohlcv[-1][4]
             datetime = exchange.iso8601(exchange.milliseconds())
             # Unpack values
             ask_price, ask_size = np.array(list(zip(*orderbook["asks"]))[0:2])
             bid_price, bid_size = np.array(list(zip(*orderbook["bids"]))[0:2])
-            spread = np.round(ask_price - bid_price, 4)
+            spread = np.round(ask_price - bid_price, 6)
+            mid_price = np.round((ask_price[0] + bid_price[0]) / 2, 6)
+            vwap = np.round(((ask_price * ask_size).sum() + (bid_price * bid_size).sum()) / (ask_size.sum() + bid_size.sum()), 6)
+            levels=len(ask_price)
             # Final data format for the results
             ob.append(
                 {
@@ -32,6 +39,10 @@ async def async_client(exchange_id, run_time: int, symbol: str):
                         "bid": bid_price.tolist(),
                         "bid_size": bid_size.tolist(),
                         "spread": spread.tolist(),
+                        "closing_price": closing_price,
+                        "mid_price": mid_price,
+                        "vwap": vwap,
+                        "levels":levels
                     },
                 }
             )
@@ -53,28 +64,26 @@ async def multi_orderbooks(exchanges, run_time: int, symbol: str):
     orderbooks = await asyncio.gather(*input_coroutines, return_exceptions=True)
     return orderbooks
 
+def dataframe(data):
+    df = pd.DataFrame(columns=['exchange', 'timestamp', 'levels', 'ask_volume', 'bid_volume', 'total_volume', 'mid_price', 'vwap'])
+    for row in range(len(data)):
+        ob = ast.literal_eval(data['orderbook'][row])
+        df.loc[row, 'exchange'] = data['exchange'][row]
+        df.loc[row, 'timestamp'] = data['datetime'][row]
+        df.loc[row, 'levels'] = ob['levels']
+        df.loc[row, 'ask_volume'] = sum(ob['ask_size'])
+        df.loc[row, 'bid_volume'] = sum(ob['bid_size'])
+        df.loc[row, 'total_volume'] = sum(ob['ask_size']) + sum(ob['bid_size'])
+        df.loc[row, 'mid_price'] = ob['mid_price']
+        df.loc[row, 'vwap'] = ob['vwap']
+    return df
 
-if __name__ == "__main__":
-    exchanges = ["bitopro", "alpaca", "bitget"]
-    run_time = 10  # seconds
-    symbol1 = "ETH/BTC"
-    symbol2 = "ETH/EUR"
-    symbol3 = "BTC/USD"
-
-    data1 = asyncio.run(multi_orderbooks(exchanges, run_time=run_time, symbol=symbol1))
-    data1 = [item for sublist in data1 for item in sublist]
-    data1 = pd.DataFrame(data1)
-
-    data2 = asyncio.run(multi_orderbooks(exchanges, run_time=run_time, symbol=symbol2))
-    data2 = [item for sublist in data2 for item in sublist]
-    data2 = pd.DataFrame(data2)
-
-    data1.append(data2)
-
-    data3 = asyncio.run(multi_orderbooks(exchanges, run_time=run_time, symbol=symbol3))
-    data3 = [item for sublist in data3 for item in sublist]
-    data3 = pd.DataFrame(data3)
-
-    data1.append(data3)
-
-    print(data1)
+def eff_spread(data):
+    df = pd.DataFrame(columns=['timestamp', 'close', 'spread', 'effective spread'])
+    for row in range(len(data)):
+        ob = ast.literal_eval(data['orderbook'][row])
+        df.loc[row, 'timestamp'] = data['datetime'][row]
+        df.loc[row, 'close'] = 0 #poner close
+        df.loc[row, 'spread'] = 0 #poner el spread
+        df.loc[row, 'effective spread'] = 0 #poner effectice pread
+    return df
