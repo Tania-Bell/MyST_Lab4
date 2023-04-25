fd949c7e4e1acab8136d31adf77857740512dbd4
gerardogonz <gera.gonzf@gmail.com>
2023-04-25 08:47:59 -0600

    cambios
    actualización de jupyter, y visualizations

---
+++ visualizations.py
@@ -0,0 +1,11 @@
+def grafica_btc(dataframe): 
+    fig=px.line(dataframe, x="timestamp", y="mid_price",color="exchange",title=(" Midprice de BTC/USDT"))
+
+    return fig.show()
+
+
+
+def grafica_eth(dataframe): 
+    fig=px.line(dataframe, x="timestamp", y="mid_price",color="exchange",title=(" Midprice de ETH/USDT"))
+
+    return fig.show()
